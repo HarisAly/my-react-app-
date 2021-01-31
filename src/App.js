87cc6ef2cf4e94f6b897e21afe2b03c6659f225d
HarisAly <haris.aly10@gmail.com>
2021-01-31 16:31:21 +0500

    Initialize project using Create React App

---
+++ src/App.js
@@ -0,0 +1,13 @@
+import logo from './logo.svg';
+import './App.css';
+
+function App() {
+  return (
+    <div>
+        Hello World!
+        How are You
+    </div>
+  );
+}
+
+export default App;
